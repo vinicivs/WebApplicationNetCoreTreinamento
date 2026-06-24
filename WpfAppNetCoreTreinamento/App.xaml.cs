bba325fb5d06083365ba02de032aff2071b91f5f
vinicivs <vinicivs@gmail.com>
2026-06-24 09:25:33 -0300

    feat: Windows forms e Console app no Net 10

---
+++ WpfAppNetCoreTreinamento/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfAppNetCoreTreinamento
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ WpfAppNetCoreTreinamento/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppNetCoreTreinamento
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Sair_Click(object sender, RoutedEventArgs e)
+        {
+            // Sair
+            Close();
+        }
+
+        private void Mensagem_Click(object sender, RoutedEventArgs e)
+        {
+            // Mensagem de alerta
+            MessageBox.Show("Mensagem de alerta", "Alerta", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+    }
+}
