ec7e35fd572d9a5c9fc3c07be035a89a69ea939e
vinicivs <vinicivs@gmail.com>
2026-06-24 11:25:17 -0300

    feat: CRUD completo no Blazor Server

---
+++ BlazorAppServerNetCoreTreinamento/Data/AppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.ConstrainedExecution;
+
+namespace BlazorAppServerNetCoreTreinamento.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Cep> Cep
+        {
+            get; set;
+        }
+    }
+
+    [Table("Cep")] // nome da tabela no banco
+    public class Cep
+    {
+        public int Id { get; set; }
+
+        [Column("Cep")]
+        [Required(ErrorMessage = "O CEP é obrigatório")]
+        [RegularExpression(@"^\d{5}-\d{3}$", ErrorMessage = "Formato válido: 00000-000")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "O logradouro é obrigatório")]
+        [StringLength(200, ErrorMessage = "Máximo de 200 caracteres")] 
+        public string Logradouro { get; set; }
+        public string? Numero { get; set; }
+        public string? Complemento { get; set; }
+
+        [Required(ErrorMessage = "O bairro é obrigatório")]
+        [StringLength(100)]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "A cidade é obrigatória")]
+        [StringLength(100)]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O estado é obrigatório")]
+        [StringLength(2, ErrorMessage = "Use a sigla do estado (ex: SP)")]
+        public string Uf { get; set; }
+    }
+}
--- BlazorAppServerNetCoreTreinamento/Program.cs
+++ BlazorAppServerNetCoreTreinamento/Program.cs
@@ -1,6 +1,7 @@
 using BlazorAppServerNetCoreTreinamento.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+// Configurar a conexão com o banco de dados
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
